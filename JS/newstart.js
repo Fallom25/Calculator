355f4b47cd16fda0b6a8ed73d1a874298b056202
DESKTOP-I8MD8U0\Forest <forest.p.sheppard@outlook.com>
2022-02-15 16:33:14 +1100

    updated readme

--- JS/newstart.js
+++ JS/newstart.js
@@ -1,27 +1,27 @@
-if(document.readyState == 'loading'){
+if (document.readyState == 'loading') {
 	document.addEventListener('DOMContentLoaded', ready);
-} else{
+} else {
 	ready();
 }
 
 const regex = /(?<group1>\-?\d+\.?\d*)(?<group2>[\*\/\+\-])(?<group3>\-?\d+\.?\d*)/;
 
-function ready(){
-    const clearBtn = document.getElementById('clear__btn');
-    clearBtn.addEventListener('click', clearClick);
+function ready() {
+	const clearBtn = document.getElementById('clear__btn');
+	clearBtn.addEventListener('click', clearClick);
 
-    const mathBtnList = document.getElementsByClassName('btn__math');
-    for(let i = 0; i < mathBtnList.length; i++){
+	const mathBtnList = document.getElementsByClassName('btn__math');
+	for (let i = 0; i < mathBtnList.length; i++) {
 		mathBtnList[i].addEventListener('click', mathClick);
 	}
 
-    const numBtnList = document.getElementsByClassName('btn__numbers');
-    for(let i = 0; i < numBtnList.length; i++){
+	const numBtnList = document.getElementsByClassName('btn__numbers');
+	for (let i = 0; i < numBtnList.length; i++) {
 		numBtnList[i].addEventListener('click', numberClick);
 	}
 
 	const equalsBtn = document.getElementById('equals__btn');
-    equalsBtn.addEventListener('click', equalsClick);
+	equalsBtn.addEventListener('click', equalsClick);
 
 	const happyBtn = document.getElementById('bonus__btn');
 	happyBtn.addEventListener('click', happyClick);
@@ -32,7 +32,7 @@ const numberClick = (event) => {
 	let output = document.getElementById('output');
 	let number = event.target.value;
 
-	if(output.innerHTML === '0'){
+	if (output.innerHTML === '0') {
 		output.innerHTML = `${number}`;
 	}
 	else {
@@ -42,8 +42,8 @@ const numberClick = (event) => {
 
 const mathClick = (event) => {
 	output.innerHTML = equationCheck(output.innerHTML);
-	if (output.innerHTML !== '0'){
-		output.innerHTML +=	event.target.innerHTML;
+	if (output.innerHTML !== '0') {
+		output.innerHTML += event.target.innerHTML;
 	}
 }
 
@@ -55,30 +55,30 @@ const equalsClick = () => {
 	output.innerHTML = equationCheck(output.innerHTML);
 }
 
-const  happyClick = () => {
+const happyClick = () => {
 	document.getElementById("img1").style.display = "block";
 	setTimeout(() => {
 		document.getElementById("img1").style.display = "none";
-	},5000);  
+	}, 5000);
 }
 
 const equationCheck = (expression) => {
 
-	if (['+', '-', '*', '/'].includes(expression[expression.length-1])){
+	if (['+', '-', '*', '/'].includes(expression[expression.length - 1])) {
 		alert("Please do not use multiple operators in a row");
 		return '0';
 	}
 
-	if((expression.match(/\./g) || []).length > 2){
+	if ((expression.match(/\./g) || []).length > 2) {
 		alert("Error too many decimal points");
 		return '0';
 	}
 
-	if (expression[expression.length-1].includes('.')) {
+	if (expression[expression.length - 1].includes('.')) {
 		expression += '00';
 	}
 
-	if (expression.match(regex) === null){
+	if (expression.match(regex) === null) {
 		return expression;
 	}
 	return calculate(expression);
@@ -91,16 +91,15 @@ const calculate = (expression) => {
 	let group3 = parseFloat(groups.group3);
 	let group2 = groups.group2;
 
-	if(group2 === '/'){
+	if (group2 === '/') {
 		return (group1 / group3).toFixed(2);
-	}else if(group2 === '*'){
+	} else if (group2 === '*') {
 		return (group1 * group3).toFixed(2);
-	}else if(group2 === '-'){
+	} else if (group2 === '-') {
 		return (group1 - group3).toFixed(2);
-	}else{
-		console.log("here");
+	} else {
 		return (group1 + group3).toFixed(2);
-	} 
+	}
 }
 
 
--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,23 @@
-## Basic HTML Calculator
-I built this as part of my Nology training.
+## Basic HTML Calculator - Project for _Nology
+
+## Project Description
+The aim of this project was to build a basic Calculator
+
+## How to Install and Run
+I use the VSCode extension LiveServer to run my HTML websites locally.
+
+## How to use
+Works the same way a simple calculator works.
+Smile button displays Surprise Pic on timer.
+
+## Outline of my Solution
+Uses HTML, CSS and JS. Uses the Sass preprocess. 
+
+## Considerations and Further Growth
+Would like to add the ability for users to enter their own delim.
+
+### Testing
+Tests are written but are not working properly with current set up.
+Window Alert is not currently being tested this is something I would like to add in the future.
+
+
